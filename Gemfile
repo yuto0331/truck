dc1cd474de80f5761c5de19669c9899190b708d9
yutokimura <yutokimura0331@gmail.com>
2018-03-21 00:45:25 +0000

    frot_top

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt','3.1.11'
 gem 'carrierwave'
 gem 'mini_magick'
+gem "font-awesome-rails"
 
 
 group :development, :test do
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -58,7 +58,7 @@ class RestaurantsController < ApplicationController
   def destroy
     @restaurant.destroy
     respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
+      format.html { redirect_to restaurants_path, notice: 'Restaurant was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
